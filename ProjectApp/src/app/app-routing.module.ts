a90227501c4fadd8b888b8c0c6a1404299fc6a32
Indira <malladi.indira1999@gmail.com>
2022-03-17 13:10:07 +0530

    changes

--- ProjectApp/src/app/app-routing.module.ts
+++ ProjectApp/src/app/app-routing.module.ts
@@ -2,22 +2,30 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddloanComponent } from './applyloan/addloan/addloan.component';
 import { ApplyloanComponent } from './applyloan/applyloan.component';
-import { LoanstatusComponent } from './loanstatus/loanstatus.component';
+import { LoanstatusComponent } from './applyloan/loanstatus/loanstatus.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './applyloan/profile/profile.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AccountComponent } from './account/account.component';
+import { AccountComponent } from './applyloan/account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { AppliedLoansComponent } from './admin/applied-loans/applied-loans.component';
+import { LoanDetailsComponent } from './admin/loan-details/loan-details.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'login-page',pathMatch:'full'},
   {path:'login-page',component:LoginPageComponent},
   {path:'Sign-up',component:SignUpComponent},
-  {path:'applyloan',component:ApplyloanComponent},
-  {path:'profile',component:ProfileComponent},
-  {path:'loanstatus',component:LoanstatusComponent},
-  {path:'addloan',component:AddloanComponent},
-  {path:'account',component:AccountComponent},
-
+  {path:'applyloan',component:ApplyloanComponent, children: [
+    {path:'profile',component:ProfileComponent},
+    {path:'loanstatus',component:LoanstatusComponent},
+    {path:'addloan', component:AddloanComponent},
+    {path:'account', component:AccountComponent},
+  ]},
+  {path:'admin', component: AdminComponent, children: [
+    {path: 'appliedloans', component: AppliedLoansComponent},
+    {path: 'loandetails', component: LoanDetailsComponent}
+  ]},
+  //{path:'**', redirectTo:'404'},
 ];
 
 @NgModule({
