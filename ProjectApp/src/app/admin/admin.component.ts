2d9d493f3ddb0987fa566e2a9f502bdbe92f9335
Afreen Aara Shaik <shaikafreenaara2001@gmail.com>
2022-03-21 17:43:08 +0530

    admin front end

---
+++ ProjectApp/src/app/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if(sessionStorage.getItem("User")!="admin"){
+      this.router.navigate(['/']);
+    }
+  }
+
+  logout(){
+    sessionStorage.removeItem("User");
+    this.router.navigate(['/login-page']);
+  }
+
+}
